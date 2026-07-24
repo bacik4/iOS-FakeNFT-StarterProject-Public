ea4f3e416c1afc87cc73158504590de9bcb6b8e4
bacik4 <igorekgk2016@gmail.com>
2026-07-24 11:40:03 +0300

    added documentation via ///

--- FakeNFT/Models/Network/NftCollection.swift
+++ FakeNFT/Models/Network/NftCollection.swift
@@ -1,13 +1,30 @@
 import Foundation
 
+/// Сетевая модель коллекции NFT, получаемая от API.
 struct NftCollection: Decodable {
+
+    /// Дата создания коллекции.
     let createdAt: String
+
+    /// Название коллекции.
     let name: String
+
+    /// URL обложки коллекции.
     let cover: URL
+
+    /// Идентификаторы NFT, входящих в коллекцию.
     let nftIds: [String]
+
+    /// Описание коллекции.
     let description: String
+
+    /// Автор коллекции.
     let author: String
+
+    /// URL сайта коллекции.
     let website: URL
+
+    /// Уникальный идентификатор коллекции.
     let id: String
 
     enum CodingKeys: String, CodingKey {
