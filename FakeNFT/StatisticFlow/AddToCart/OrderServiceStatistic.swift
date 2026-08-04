6ea9ea07bb6e317b3adcc01a8faee3b787dbe91d
Jagernout14 <jagernout@icloud.com>
2026-08-05 01:10:51 +0300

    fix(MergeStatisticFlow): Восстановить работоспособность приложения
    Комментарии отсутствуют

--- FakeNFT/StatisticFlow/AddToCart/OrderServiceStatistic.swift
+++ FakeNFT/StatisticFlow/AddToCart/OrderServiceStatistic.swift
@@ -9,7 +9,7 @@ import Foundation
 
 typealias OrderCompletion = (Result<Order, Error>) -> Void
 
-protocol OrderService {
+protocol OrderServiceStatistic {
     
     @discardableResult func loadOrder(completion: @escaping OrderCompletion) -> NetworkTask?
     @discardableResult func updateOrder(nfts: [String], completion: @escaping OrderCompletion) -> NetworkTask?
